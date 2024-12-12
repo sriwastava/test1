204a6e89618c97dfec4b1d1e9895f58179589b40
vks <sriwastava@gmail.com>
2024-12-12 11:52:18 -0600

    refactored CMakeLists.txt to generate 2 binaries

--- tests/mytest1.cpp
+++ tests/mytest1.cpp
@@ -1,17 +1,15 @@
 #include "utest.h"
 
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
+// int fibonacci(int n) {
+//     if (n <= 1) {
+//         return n;
+//     }
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+// }
 
 UTEST(foo, bar) {
   ASSERT_TRUE(1);
 }
 
-UTEST(fibonacci, test1) {
-  ASSERT_EQ(fibonacci(1), 2);
-}
+
 
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,5 +1,12 @@
 #include "utest.h"
+#include "../fibonacci.h"
 
-UTEST(foo2, bar) {
-  ASSERT_TRUE(1);
+
+UTEST(fibonacci, test1) {
+  ASSERT_EQ(fibonacci(0), 0);
+  ASSERT_EQ(fibonacci(1), 1);
+  ASSERT_EQ(fibonacci(2), 1);
+  ASSERT_EQ(fibonacci(3), 2);
+  ASSERT_EQ(fibonacci(9), 34);
 }
+
